4831d8608b207c4c73fbef66a9859a5adc2cdec6
Ajit Krishnamoorthy <ajit.krish18@gmail.com>
2023-11-04 11:29:43 -0500

    Leetcode 215 11/4

---
+++ 215_kthLargestElementArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for x in nums[k:]:
+            if x > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, x)
+        return heap[0]
