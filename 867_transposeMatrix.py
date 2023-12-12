a835943170a0ad5f5e7a285d63b508ff53ea7b18
Ajit Krishnamoorthy <ajit.krish18@gmail.com>
2023-12-11 21:57:21 -0600

    Leetcode 867 12/11

---
+++ 867_transposeMatrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        transArray = [[0]*(len(matrix)) for _ in range(len(matrix[0]))]
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                transArray[c][r] = matrix[r][c]
+        return transArray
